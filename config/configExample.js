40cb26a5b0a05c56adf0af32da9502e68ebac286
shane <haroboy876@gmail.com>
2016-05-03 23:28:06 -0700

    fix configExample

--- config/configExample.js
+++ config/configExample.js
@@ -3,3 +3,7 @@
 const username = "YOURUSERNAME";
 const password = "oauth:j8998jJjknJNkjNjkn8677BJHbjhbjhb8";
 const channels = ["#CHANNELONE", "#CHANNELTWO"];
+
+exports.username = username;
+exports.password = password;
+exports.channels = channels;
